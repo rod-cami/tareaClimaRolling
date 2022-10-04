d3909d3a43a40e070d1e85d29302e683a332869b
rod-cami <camilarodriguez4646@gmail>
2022-10-04 10:57:15 -0300

    Sigue pateado pero mas tarde lo pongo mas bonito

--- app/app.js
+++ app/app.js
@@ -7,9 +7,9 @@
 //   .then(json => console.log(json))
 
 const getDatosCiudad = async (lat,lon) => {
-  const resultado = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7fb5c1a57477a9413791eceb21f426c2`);
+  const resultado = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7fb5c1a57477a9413791eceb21f426c2&units=metric&lang=es`);
   const datos = await resultado.json();
-  return console.log(datos)
+  return datos
 }
 
 const getCoordenadas = async (ciudad,pais) => {
@@ -28,7 +28,65 @@ const getDatos = async (ciudad,pais) => {
 
   })
   const datosObj = await getDatosCiudad(lat,lon);
-  return 0
+  console.log('hola');
+  console.log(datosObj);
+  return datosObj
 }
 
 getDatos('Salta','AR');
+
+const setDatos = async () => {
+  let ciudad = document.getElementById('nombreCiudad').value;
+  let codigo = document.getElementById('codPais').value;
+  let codigoImg,textClima;
+  const div = document.getElementById('card');
+  const div1 = document.getElementById('minima');
+  const div2 = document.getElementById('maxima');
+
+  const datosClima = await getDatos(ciudad,codigo);
+
+  const datImg = (datosClima.weather).map(cod => {
+    codigoImg = cod.icon;
+    textClima = cod.description;
+  })
+
+  const card = (
+    `
+    <div class="card text-center" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title fs-1 fw-bold">${datosClima.name}</h5>
+        <img src="https://openweathermap.org/img/wn/${codigoImg}@2x.png" class="card-img w-25" alt="...">
+        <p class="card-text fs-1 fw-bold">${datosClima.main.temp}°C</p>
+        <p class="card-text">${textClima}</p>
+      </div>
+    </div>
+    `
+  )
+
+  const card1 = (
+    `
+    <div class="card text-center" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Minima</h5>
+        <p class="card-text">${datosClima.main.temp_min}°C</p>
+      </div>
+    </div>
+    `
+  )
+
+  const card2 = (
+    `
+    <div class="card text-center" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Maxima</h5>
+        <p class="card-text">${datosClima.main.temp_max}°C</p>
+      </div>
+    </div>
+    `
+  )
+
+  div.innerHTML = card;
+  div1.innerHTML = card1;
+  div2.innerHTML = card2;
+}
+
